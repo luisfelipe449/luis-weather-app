7af5140b0b5802810050b4677b7575c79c9c76b2
Luis Felipe <1luisfelipe2@gmail.com>
2022-03-30 20:22:07 -0400

    durante a aula

--- src/App.js
+++ src/App.js
@@ -14,11 +14,16 @@ function App() {
         .get(url)
         .then((response) => {
           setData(response.data);
+          setLocation("");
         })
         .catch((err) => {
           setError(err.data);
+          console.log(error);
+          setLocation("Location do not exist");
+          setTimeout(() => {
+            setLocation(event.target.value);
+          }, 3000);
         });
-      setLocation("");
     }
   };
 
@@ -29,52 +34,60 @@ function App() {
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
-          placeholder="Enter Location"
+          placeholder="Enter New Location"
           type="text"
         />
       </div>
-      <div className="container">
-        <div className="top">
-          
-
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? (
-              <h1>{(((data.main.temp - 32) * 5) / 9).toFixed()}°C</h1>
-            ) : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+      {searchLocation === undefined ? (
+        <div className="container">
+          <h1>Erro</h1>
         </div>
-
-        {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
+      ) : null }
+      {searchLocation !== undefined && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
               {data.main ? (
-                <p className="bold">
-                  {(((data.main.feels_like - 32) * 5) / 9).toFixed()}°C
-                </p>
+                <h1>{(((data.main.temp - 32) * 5) / 9).toFixed()}°C</h1>
               ) : null}
-              <p>Feels Like</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? (
-                <p className="bold">
-                  {(data.wind.speed * 1.609).toFixed()} KM/h
-                </p>
-              ) : null}
-              <p>Wind Speed</p>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
             </div>
           </div>
-        )}
-      </div>
+
+          {data.name !== undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">
+                    {(((data.main.feels_like - 32) * 5) / 9).toFixed()}°C
+                  </p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">
+                    {(data.wind.speed * 1.609).toFixed()} KM/h
+                  </p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+     
     </div>
   );
 }
